715c188e981a96e4402592103969024bfe34f4a6
jinsk9268 <jinsk9268@gmail.com>
2024-12-22 19:28:42 +0900

    chore: Github Pages 자동 배포를 위한 Github Actions 설정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+	base: "/text-fireworks/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
